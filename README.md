cb045fe7d4cf3c6dd4ef9f56f2c2284693c49a08
alextanhongpin <alextan220990@gmail.com>
2020-09-23 07:35:41 +0200

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# vue-3-starter
+
+Testing out the new Vue3 and the _composition api_.
+
+```bash
+$ npm init vite <app-name>
+```
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <button @click="increment">{{ count }}</button>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  setup () {
+    const count = ref(0)
+    const increment = () => {
+      count.value++
+    }
+    return {
+      count,
+      increment
+    }
+  }
+}
+</script>
